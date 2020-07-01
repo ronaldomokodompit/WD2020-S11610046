6e2a64d2d187cfea3ba433c0e780f9d1101b3763
ronaldomokodompit <mokodompitronaldo1@gmail.com>
2020-07-01 17:39:13 +0800

    menambahkah pertemuan13

---
+++ Pertemuan18/index.js
@@ -0,0 +1,73 @@
+// // variable dan tipe data 1
+// console.log ("Hello World");
+
+// let firstname = "John"
+// let $number = 3;
+// let year = 
+// var age = 20;
+// let isMarried = false;
+
+// firstname = "jhon"
+
+// console.log ( firstname);
+// console.log ($number);
+// console.log (isMarried);
+// console.log (years);
+
+// // operator Aritmatika
+
+// let num1 = 10;
+// let num2 = 2;
+
+// console.log (num1 + num2);
+// console.log (num1 - num2);
+// console.log (num1 * num2);
+// console.log (num1 / num2);
+// console.log (num1 % num2);
+// console.log (num1 ** num2);
+// num1 ++;
+// console.log (num1);
+// num1 --;
+// console.log (num1);
+
+// //Assignment Operator
+
+// let num3 = 10;
+// let num4;
+//  num4 = num3
+//  console.log (num4);
+
+//  num3 **= 2;
+//  console.log (num3;
+
+// //String Operator
+
+// let firstname = "john"
+// let lastname = "doe"
+// let age = "28"
+
+// console.log (firstname + " " + lastname);
+// console.log (firstname + age);
+
+//Operator Perbandinga
+
+let num = 10;
+
+console.log (num1 == 10);
+console.log (num1 "10");
+
+console.log (num1 === 10);
+console.log (num1 === "10");
+
+console.log (num1 >= 10);
+console.log (num1 <= 10);
+
+let num2 = num1 === 10 ? "ten" : "Another Number";
+console.log (num2);
+
+// Operator Logika
+
+let hasCertificate = true;
+let hasTranscript =true;
+
+console.log (hasCertificate && hasTranscript);
